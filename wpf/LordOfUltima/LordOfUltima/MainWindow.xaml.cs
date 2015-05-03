46cb346af63821a4344dab75cc041553761b0a5c
FelixLeChat <felixlrc21@gmail.com>
2015-05-03 16:50:11 -0400

    ajout barre au haut

--- wpf/LordOfUltima/LordOfUltima/MainWindow.xaml.cs
+++ wpf/LordOfUltima/LordOfUltima/MainWindow.xaml.cs
@@ -27,52 +27,14 @@ namespace LordOfUltima
         {
             InitializeComponent();
 
-            // Couleur du menu
-            top_menu.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xE9, 0xD3, 0xAE));
-            grid.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xE9, 0xD3, 0xAE));
-
-            // Background pour canvas
-            ImageBrush imageBrush = new ImageBrush();
-            imageBrush.ImageSource = new BitmapImage(new Uri(@"Media/main.png", UriKind.Relative));
-            canvas1.Background = imageBrush;
-
-
-            LinearGradientBrush myLinearGradientBrush = new LinearGradientBrush();
-            myLinearGradientBrush.StartPoint = new Point(0, 0);
-            myLinearGradientBrush.EndPoint = new Point(0, 1);
-
-            myLinearGradientBrush.GradientStops.Add( new GradientStop(Colors.White, -0.03));
-            myLinearGradientBrush.GradientStops.Add( new GradientStop(Colors.Transparent, 0.10));
-            myLinearGradientBrush.GradientStops.Add( new GradientStop(Colors.Transparent, 0.90));
-            myLinearGradientBrush.GradientStops.Add( new GradientStop(Colors.White, 1.03));
-
-            canvas_fog.Fill = myLinearGradientBrush;
-
-
-            // Backgrouound pour Scroll View
-            ImageBrush imageScroll = new ImageBrush();
-            imageScroll.ImageSource = new BitmapImage(new Uri(@"Media/menu.png", UriKind.Relative));
-            scrollview.Background = imageScroll;
-
-            // Image for wood 
-            ImageBrush imageRWood = new ImageBrush();
-            imageRWood.ImageSource = new BitmapImage(new Uri(@"Media/ressource/icon/Lou_resource_wood.png", UriKind.Relative));
-            ress_wood.Background = imageRWood;
-
-            // Image for stone
-            ImageBrush imageRStone = new ImageBrush();
-            imageRStone.ImageSource = new BitmapImage(new Uri(@"Media/ressource/icon/Lou_resource_stone.png", UriKind.Relative));
-            ress_stone.Background = imageRStone;
-
-            ImageBrush ressMenu = new ImageBrush();
-            ressMenu.ImageSource = new BitmapImage(new Uri(@"Media/ress_menu.png", UriKind.Relative));
-            ress_menu.Background = ressMenu;
+            initImages();
 
             // Set the gameboard Instance
             m_gameboard = Gameboard.getInstance();
             // Insertion des elements dans la carte
             insertMap();
-            
+            // Inserer les elements dans le menu
+            insertMenu();
         }
 
         private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
@@ -94,7 +56,68 @@ namespace LordOfUltima
 
         private void insertMenu()
         {
+            // TODO : Insertion des elements graphique dans le menu
+        }
 
+        private void initImages()
+        {
+
+            // Couleur du menu
+            ImageBrush imageMenuRepeat = new ImageBrush();
+            imageMenuRepeat.ImageSource = new BitmapImage(new Uri(@"Media/menu_repeat.png", UriKind.Relative));
+            /*imageMenuRepeat.TileMode = TileMode.FlipX;
+            imageMenuRepeat.Stretch = Stretch.Uniform;
+            imageMenuRepeat.Viewport = new Rect(0, 0, 92, 61);
+            imageMenuRepeat.ViewportUnits = BrushMappingMode.Absolute;*/
+            top_menu.Background = imageMenuRepeat;
+
+            grid.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xE9, 0xD3, 0xAE));
+
+            // Background pour canvas
+            ImageBrush imageBrush = new ImageBrush();
+            imageBrush.ImageSource = new BitmapImage(new Uri(@"Media/main.png", UriKind.Relative));
+            canvas1.Background = imageBrush;
+
+            // Background pour le degrade (Fog)
+            LinearGradientBrush myLinearGradientBrush = new LinearGradientBrush();
+            myLinearGradientBrush.StartPoint = new Point(0, 0);
+            myLinearGradientBrush.EndPoint = new Point(0, 1);
+            myLinearGradientBrush.GradientStops.Add(new GradientStop(Colors.White, -0.03));
+            myLinearGradientBrush.GradientStops.Add(new GradientStop(Colors.Transparent, 0.10));
+            myLinearGradientBrush.GradientStops.Add(new GradientStop(Colors.Transparent, 0.90));
+            myLinearGradientBrush.GradientStops.Add(new GradientStop(Colors.White, 1.03));
+            canvas_fog.Fill = myLinearGradientBrush;
+
+
+            // Background pour Scroll View
+            ImageBrush imageScroll = new ImageBrush();
+            imageScroll.ImageSource = new BitmapImage(new Uri(@"Media/menu.png", UriKind.Relative));
+            scrollview.Background = imageScroll;
+
+            // Image pour le menu des ressources
+            ImageBrush ressMenu = new ImageBrush();
+            ressMenu.ImageSource = new BitmapImage(new Uri(@"Media/ress_menu.png", UriKind.Relative));
+            ress_menu.Background = ressMenu;
+
+            // Image for wood 
+            ImageBrush imageRWood = new ImageBrush();
+            imageRWood.ImageSource = new BitmapImage(new Uri(@"Media/ressource/icon/Lou_resource_wood.png", UriKind.Relative));
+            ress_wood.Background = imageRWood;
+
+            // Image for stone
+            ImageBrush imageRStone = new ImageBrush();
+            imageRStone.ImageSource = new BitmapImage(new Uri(@"Media/ressource/icon/Lou_resource_stone.png", UriKind.Relative));
+            ress_stone.Background = imageRStone;
+
+            // Image for grain
+            ImageBrush imageRGrain = new ImageBrush();
+            imageRGrain.ImageSource = new BitmapImage(new Uri(@"Media/ressource/icon/Lou_resource_grain.png", UriKind.Relative));
+            ress_grain.Background = imageRGrain;
+
+            // Image for iron
+            ImageBrush imageRIron = new ImageBrush();
+            imageRIron.ImageSource = new BitmapImage(new Uri(@"Media/ressource/icon/Lou_resource_iron.png", UriKind.Relative));
+            ress_iron.Background = imageRIron;
         }
 
 
