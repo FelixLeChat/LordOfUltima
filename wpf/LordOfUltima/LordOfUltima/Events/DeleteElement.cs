ab7e49f3a26804bfb5b53437dde3c67127277b94
FelixLeChat <felixlrc21@gmail.com>
2015-06-10 21:24:49 -0400

    fix delete fields when farm is deletes

--- wpf/LordOfUltima/LordOfUltima/Events/DeleteElement.cs
+++ wpf/LordOfUltima/LordOfUltima/Events/DeleteElement.cs
@@ -22,8 +22,14 @@ namespace LordOfUltima.Events
                 _elementToDelete.GetElementType().GetElementType() == ElementType.Type.BUILDING_TOWNHALL) 
                 return;
 
+            if (_elementToDelete.GetElementType().GetElementType() == ElementType.Type.BUILDING_FARM)
+            {
+                resetElementNear(_elementToDelete);
+            }
 
             _elementToDelete.Initialise();
+
+            
 
             // set the neighbouring ressources count
             RessourcesBuildingCheck.Instance.cheakAllNeighbourRessources();
@@ -37,6 +43,85 @@ namespace LordOfUltima.Events
             // handle level indicator
             LevelIndicatorVisibility.Instance.HideLevelIndicator();
             LevelIndicatorVisibility.Instance.HandleLevelIndicatorVisibility();
+        }
+
+        private void resetElementNear(Element element)
+        {
+            if (element == null || !element.HasElement || element.GetElementType() == null)
+                return;
+
+            Element[,] map = Gameboard.Instance.GetMap();
+            int frameCount = Gameboard.Instance.GetFrameCount;
+            int x = element.PositionX;
+            int y = element.PositionY;
+
+            if (y > 0)
+            {
+                if (x > 0)
+                {
+                    if (!map[x - 1, y - 1].HasElement)
+                    {
+                        deleteElement((x - 1), (y - 1));
+                    }
+                }
+                if (!map[x, y - 1].HasElement)
+                {
+                    deleteElement(x, y - 1);
+                }
+
+                if (x < frameCount - 1)
+                {
+                    if (!map[x + 1, y - 1].HasElement)
+                    {
+                        deleteElement(x + 1, y - 1);
+                    }
+                }
+
+            }
+            if (y < frameCount - 1)
+            {
+                if (x > 0)
+                {
+                    if (!map[x - 1, y + 1].HasElement)
+                    {
+                        deleteElement(x - 1, y + 1);
+                    }
+                }
+
+                if (!map[x, y + 1].HasElement)
+                {
+                    deleteElement(x, y + 1);
+                }
+
+                if (x < frameCount - 1)
+                {
+                    if (!map[x + 1, y + 1].HasElement)
+                    {
+                        deleteElement(x + 1, y + 1);
+                    }
+                }
+            }
+
+            if (x > 0)
+            {
+                if (!map[x - 1, y].HasElement)
+                {
+                    deleteElement(x - 1, y);
+                }
+            }
+            if (x < frameCount - 1)
+            {
+                if (!map[x + 1, y].HasElement)
+                {
+                    deleteElement(x + 1, y);
+                }
+            }
+        }
+
+        private void deleteElement(int x, int y)
+        {
+            Element[,] map = Gameboard.Instance.GetMap();
+            map[x, y].Initialise();
         }
     }
 }
